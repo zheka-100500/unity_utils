5c6702803c3ac451173b231c134b7267ee8dd58c
Eliyar Curuxa <zheka_100500@mail.ru>
2024-09-15 18:53:53 +0300

    Create CameraConstantWidthVersion2.cs

---
+++ CameraConstantWidthVersion2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+namespace Game
+{
+    [ExecuteAlways]
+    public class CameraConstantWidthVersion2 : MonoBehaviour
+    {
+        [SerializeField] private float defaultSize = 5f;
+        [SerializeField] private float scaledSize = 5f;
+        [SerializeField] private Camera cam;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            if(cam == null) return;
+            
+            
+            cam.orthographicSize = Screen.width < Screen.height ? scaledSize : defaultSize;
+        }
+
+  
+    }
+}
