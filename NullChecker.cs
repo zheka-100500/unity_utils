90141c285b1494c177e90fe9f5d19081b5288937
Eliyar Curuxa <zheka_100500@mail.ru>
2024-09-15 19:00:20 +0300

    Update NullChecker.cs

--- NullChecker.cs
+++ NullChecker.cs
@@ -1,31 +1,32 @@
 using System;
-
 #if UNITY_EDITOR
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
-
 [InitializeOnLoad]
 public static class NullChecker
 {
    static NullChecker()
    {
-     
       EditorApplication.playModeStateChanged += EditorApplicationOnplayModeStateChanged;
+      SceneManager.activeSceneChanged += SceneManagerOnactiveSceneChanged;
    }
-
+   private static void SceneManagerOnactiveSceneChanged(Scene arg0, Scene arg1)
+   {
+      if (EditorApplication.isPlaying) CheckAllForNull(arg1.GetRootGameObjects());
+   }
    private static void EditorApplicationOnplayModeStateChanged(PlayModeStateChange state)
    {
-      if (state != PlayModeStateChange.EnteredPlayMode) return;
-      if(CheckAllForNull()) return;
-      EditorApplication.ExitPlaymode();
+      if (state == PlayModeStateChange.EnteredPlayMode) CheckAllForNull(SceneManager.GetActiveScene().GetRootGameObjects());
    }
-
-   private static bool CheckAllForNull()
+   private static void CheckAllForNull(GameObject[] root)
    {
-      var root = SceneManager.GetActiveScene().GetRootGameObjects();
+      var bindingFlags = BindingFlags.Instance |
+                         BindingFlags.NonPublic |
+                         BindingFlags.Public;
       var components = new List<MonoBehaviour>();
       foreach (var t in root)
       {
@@ -34,35 +35,27 @@ public static class NullChecker
       }
       foreach (var component in components)
       {
-         var fields = component.GetType().GetFields().Where(f => Attribute.IsDefined(f, typeof(CheckForNull)));
+         var fields = component.GetType().GetFields(bindingFlags).Where(f => Attribute.IsDefined(f, typeof(CheckForNull)));
          foreach (var field in fields)
          {
-
-            var value = $"{field.GetValue(component)}";
-            if (!value.Equals("null", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(value))continue;
-            Debug.LogError($"Detected null at: {component.GetType().FullName}::{field.Name}");
+            var value = field.GetValue(component);
+            if (value != null && !value.ToString().Equals("null", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrEmpty(value.ToString()))continue;
+            Debug.LogError($"Detected null at: ({component.gameObject.name}) {component.GetType().FullName}::{field.Name}");
             EditorApplication.Beep();
             if (EditorUtility.DisplayDialog("Check failed",
-                   $"Detected null in: {component.GetType().FullName}::{field.Name}, continue anyway?", "continue",
+                   $"Detected null at: ({component.gameObject.name}) {component.GetType().FullName}::{field.Name}, continue anyway?", "continue",
                    "abort"))
             {
                continue;
             }
             Selection.activeGameObject = component.gameObject;
-            Debug.LogError("Abort startup!");
-            return false;
+            Debug.LogError("Aborting!");
+            EditorApplication.ExitPlaymode();
+            return;
          }
-         
       }
-      Debug.Log("All checks passed, start allowed :)");
-      return true;
+      Debug.Log("All checks passed :)");
    }
 }
-
-
 #endif
-
-public class CheckForNull : Attribute
-{
-   
-}
+public class CheckForNull : Attribute { }
